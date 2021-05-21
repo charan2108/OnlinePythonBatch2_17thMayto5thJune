eb388b27e866f2f28837a1dfd8d8ef2494662bc0
charan2108 <sumacharan.adabala@gmail.com>
2021-05-21 19:30:10 +0530

    Create task.py

---
+++ Day5-21-05-2021(Conditions,Loops)/task.py
@@ -0,0 +1,10 @@
+
+student = input("Enter name")
+marks   = int(input("marks"))
+
+if marks>90:
+ int("the student scored distinction")
+elif marks>70:
+    print("The student scored first class")
+else:
+    print("student failed")
